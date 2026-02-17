febc9ad450d08703e64d972f3b34133cdd767737
Σmanuele <emanuele.cristoforoni@gmail.com>
2026-02-17 09:56:04 +0100

    Add initial website files

---
+++ script.js
@@ -0,0 +1,38 @@
+const navbar = document.getElementById('navbar')
+const openButton = document.getElementById('open-sidebar')
+
+
+const media = window.matchMedia("(width < 649px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e){
+    const isMobile = e.matches
+    if(isMobile){
+        navbar.setAttribute('inert', '')
+    }
+    else{
+        navbar.removeAttribute('inert')
+    }
+}
+
+function openSidebar(){
+    navbar.classList.add('show')
+    openButton.setAttribute('aria-expanded', 'true')
+    navbar.removeAttribute('inert', '')
+}
+
+function closeSidebar(){
+    navbar.classList.remove('show')
+    openButton.setAttribute('aria-expanded', 'false')
+    navbar.setAttribute('inert')
+}
+
+const navLinks = document.querySelectorAll('nav a')
+navLinks.forEach(link => { 
+    link.addEventListener('click', () => {
+        closeSidebar()
+    })
+})
+
+updateNavbar(media)
